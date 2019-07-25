c066d81644f408d17d6583aea2351fc7cc390775
z851103999 <851103999@qq.com>
2019-07-25 19:59:19 +0800

    1

--- pages/tomato/tomato.js
+++ pages/tomato/tomato.js
@@ -1,12 +1,13 @@
 Page({
 
     data: {
-        defalutSecond: 10,    //默认秒数 25分钟*60秒
+        defalutSecond: 1500,    //默认秒数 25分钟*60秒
         time: "",     //渲染的字符串
         timer: null,    //定时器数字
         timerStatus: 'stop',
         confirmVisible: false,
-        againButtonVisible: false//再来一组默认false
+        againButtonVisible: false, //再来一组默认false
+        finishConfirmVisible: false//弹窗默认隐藏
     },
 
     onShow: function () {
@@ -21,6 +22,7 @@ Page({
             this.changeTime();     //调用方法输出字符串
             if (this.data.defalutSecond <= 0) {     //如果等于00:00清除定时器
                 this.setData({ againButtonVisible: true })//如果时间等于0触发再来一组
+                this.setData({ finishConfirmVisible: true })//时间结束出弹框
                 return this.clearTimer()
             }
         }, 1000)
@@ -48,15 +50,29 @@ Page({
     },
     showConfirm() { //放弃弹框
         this.setData({ confirmVisible: true })
+        this.clearTimer()//暂停时间
     },
     hideConfirm() { //放弃弹框
         this.setData({ confirmVisible: false })
     },
     againTimer() { //放弃按钮
-        this.data.defalutSecond = 10
+        this.data.defalutSecond = 1500
         console.log(this.data.defalutSecond)
         this.setData({ againButtonVisible: false })
         this.startTimer()
+    },
+    confirmAbandon(event){
+        let content = event.detail
+        wx.navigateBack({
+            to: -1
+        });
+    },
+    confirmFinish(event) {
+         let content = event.detail
+    },
+    confirmCancel() {
+        this.setData({finishConfirmVisible: false})
     }
+
 });
 
