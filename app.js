10896c5fc4ea749f217ed0a966cb7f8ea26a8944
z851103999 <851103999@qq.com>
2019-07-30 21:28:34 +0800

    111
    111

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-      host: "https://gp-server.hunger-valley.com", 
-      app_id: "wx58da79685f605758",
-      app_secret: "ca9b121d979a937bde9c85a5e97d409c",
-      t_app_id: "jsj1877ArBmg6gFXvknDvPWv",
-      t_app_secret: "wvEzA6fbsHoyiGAqnU3Pyauy"
-  }
- })
